7b40a938f69ed59effe82ec1a2420563a3443863
Илья <ilya@MacBook-Pro-Ila.local>
2022-12-17 11:48:27 +0300

    Развернул проект, подключил шрифты, favicon, объявил переменные цветов

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- ! favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="manifest" href="favicon/site.webmanifest">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+	<meta name=description content="Тестовое задание в компанию LIFE PAY">
+	<!-- Предзагрузка шрифта -->
+	<link rel="preload" href="fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin>
+	<!-- Подключение файлов CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Алексеенков Илья - тестовое задание</title>
+</head>
+
+<body>
+	
+
+	<!-- <script src="js/bundle.js"></script> -->
+</body>
+
+</html>
