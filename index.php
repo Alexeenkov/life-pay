5130b54f2c736087f8ba89b7b47e392cb14d9ecb
Илья <ilya@MacBook-Pro-Ila.local>
2022-12-17 14:23:05 +0300

    Начал верстку проекта по принципу mobile-first. Сверстал первый блок "Мобильный эквайринг с комиссией"

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name=description content="Тестовое задание в компанию LIFE PAY">
 	<!-- ! favicon -->
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
@@ -12,8 +13,6 @@
 	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
-
-	<meta name=description content="Тестовое задание в компанию LIFE PAY">
 	<!-- Предзагрузка шрифта -->
 	<link rel="preload" href="fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin>
 	<!-- Подключение файлов CSS -->
@@ -23,9 +22,20 @@
 </head>
 
 <body>
-	
+	<main class="wrapper">
+		<div class="first-screen">
+			<h1 class="first-screen__title title"> Мобильный эквайринг с комиссией <span>от 1,2%</span> </h1>
+			<div class="first-screen__subtitle"> Принимайте оплату на выезде — любым удобным способом </div>
+			<picture class="first-screen__img">
+				<source type="image/webp" srcset="img/webp/calculator_1x.webp, img/webp/calculator_2x.webp 2x">
+				<source type="image/jpeg" srcset="img/calculator_1x.png, img/calculator_2x.png 2x">
+				<img src="img/calculator_1x.png" alt="Мобильный эквайринг с комиссией от 1,2%" title="Мобильный эквайринг с комиссией от 1,2%">
+			</picture>
+			<button class="first-screen__button button"> Подключить </button>
+		</div>
+	</main>
 
-	<!-- <script src="js/bundle.js"></script> -->
+	<script src="lib/js/jquery-3.6.2.min.js"></script>
 </body>
 
 </html>
